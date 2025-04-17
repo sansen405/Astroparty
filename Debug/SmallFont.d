d69a3c0a83ae9f9c688a3e3ff7aa8b765830fdbf
sansen405 <sanjay.senthil405@gmail.com>
2025-04-16 21:06:54 -0500

    added clock

--- Debug/SmallFont.d
+++
@@ -1,7 +0,0 @@
-# FIXED
-
-SmallFont.o: ../SmallFont.cpp \
- /Users/sanjaysenthil/Desktop/319K/MSPM0_ValvanoWare/ECE319K_Lab9H/SmallFont.h \
- ../../inc/ST7735.h
-/Users/sanjaysenthil/Desktop/319K/MSPM0_ValvanoWare/ECE319K_Lab9H/SmallFont.h:
-../../inc/ST7735.h:
